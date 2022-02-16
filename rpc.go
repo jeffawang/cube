a99d1b4cc6ed0377b86487db9d16ab02b6e6a84d
Jeff Wang <jeffawang@gmail.com>
2022-02-16 13:17:30 -0800

    game.go

--- rpc.go
+++ rpc.go
@@ -1,27 +1,122 @@
 package main
 
-import "errors"
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
 
-type Args struct {
-	A, B int
-}
+	tcell "github.com/gdamore/tcell/v2"
+)
 
-type Quotient struct {
-	Quo, Rem int
-}
+// const sockPath = "./test.sock"
 
-type Arith int
-
-func (t *Arith) Multiply(args *Args, reply *int) error {
-	*reply = args.A * args.B
-	return nil
-}
-
-func (t *Arith) Divide(args *Args, quo *Quotient) error {
-	if args.B == 0 {
-		return errors.New("divide by zero")
+func mustScreen() tcell.Screen {
+	defaultStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
 	}
-	quo.Quo = args.A / args.B
-	quo.Rem = args.A % args.B
-	return nil
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	s.SetStyle(defaultStyle)
+	s.EnableMouse()
+	s.EnablePaste()
+	s.Clear()
+	return s
+}
+
+type Drawer interface {
+	Draw(tcell.Screen)
+}
+
+type drawers []Drawer
+
+func (ds drawers) Draw(s tcell.Screen) {
+	for _, d := range ds {
+		d.Draw(s)
+	}
+}
+
+func blah() {
+	s := mustScreen()
+
+	tile := NewTile()
+	player := NewPlayer()
+
+	entities := drawers{&tile, &player}
+	entities.Draw(s)
+
+	// tile.Draw(s)
+
+	// Event loop
+	// ox, oy := -1, -1
+	cleanupOnce := sync.Once{}
+	defer cleanupOnce.Do(s.Fini)
+
+	// Hot loop
+hot:
+	for {
+		entities.Draw(s)
+		// Update screen
+		s.Show()
+
+		// Poll event
+		ev := s.PollEvent()
+
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			switch ev.Key() {
+			case tcell.KeyEscape, tcell.KeyCtrlC:
+				cleanupOnce.Do(s.Fini)
+				break hot
+			case tcell.KeyCtrlL:
+				s.Sync()
+			case tcell.KeyLeft:
+				player.Move(-1, 0)
+			case tcell.KeyRight:
+				player.Move(1, 0)
+			case tcell.KeyUp:
+				player.Move(0, -1)
+			case tcell.KeyDown:
+				player.Move(0, 1)
+			default:
+				switch ev.Rune() {
+				case 'C', 'c':
+					panic("omg")
+				case 'x':
+					player.Insert(&tile, 'x')
+				}
+			}
+		case *tcell.EventMouse:
+			// x, y := ev.Position()
+			button := ev.Buttons()
+			// Only process button events, not wheel events
+			button &= tcell.ButtonMask(0xff)
+
+			// if button != tcell.ButtonNone && ox < 0 {
+			// 	ox, oy = x, y
+			// }
+			// switch ev.Buttons() {
+			// case tcell.ButtonNone:
+			// 	if ox >= 0 {
+			// 		label := fmt.Sprintf("%d,%d to %d,%d", ox, oy, x, y)
+			// 		drawBox(s, ox, oy, x, y, boxStyle, label)
+			// 		ox, oy = -1, -1
+			// 	}
+			// }
+		}
+	}
+
+	return
+	fmt.Println(os.Args)
+	if len(os.Args) < 2 || os.Args[1] == "server" {
+		server()
+	} else {
+		client()
+	}
 }
