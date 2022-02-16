fe696225bfc6bfbbc3979c722146e440453138ec
Jeff Wang <jeffawang@gmail.com>
2022-02-15 17:59:02 -0800

    basic termloop test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jeffawang/cube
 
 go 1.17
+
+require (
+	github.com/JoelOtter/termloop v0.0.0-20210806173944-5f7c38744afb
+	github.com/nsf/termbox-go v1.1.1 // indirect
+)
--- main.go
+++ main.go
@@ -7,11 +7,50 @@ import (
 	"io"
 	"net"
 	"os"
+
+	tl "github.com/JoelOtter/termloop"
 )
 
 const sockPath = "./test.sock"
 
+type Player struct {
+	*tl.Entity
+}
+
+func (p *Player) Tick(event tl.Event) {
+	if event.Type == tl.EventKey {
+		x, y := p.Position()
+		switch event.Key {
+		case tl.KeyArrowRight:
+			p.SetPosition(x+1, y)
+		case tl.KeyArrowLeft:
+			p.SetPosition(x-1, y)
+		case tl.KeyArrowUp:
+			p.SetPosition(x, y-1)
+		case tl.KeyArrowDown:
+			p.SetPosition(x, y+1)
+		}
+	}
+}
+
 func main() {
+	game := tl.NewGame()
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorGreen,
+		Fg: tl.ColorBlack,
+		Ch: 'v',
+	})
+	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+
+	player := Player{tl.NewEntity(1, 1, 1, 1)}
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	level.AddEntity(&player)
+
+	game.Screen().SetLevel(level)
+
+	game.Start()
+
+	return
 	fmt.Println(os.Args)
 	if len(os.Args) < 2 || os.Args[1] == "server" {
 		server()
