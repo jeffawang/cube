77a93b44b155843084c555b22a40f5a47676965f
Jeff Wang <jeffawang@gmail.com>
2022-02-17 21:51:17 -0800

    broadcast replacement commands!

--- server.go
+++ server.go
@@ -7,14 +7,18 @@ import (
 )
 
 type Server struct {
-	Tile Tile
+	Tile         Tile
+	broadcastIn  chan interface{}
+	broadcastOut []chan interface{}
 }
 
 func NewServer() *Server {
 	tile := NewTile()
 	tile.Cells[3][3].Rune = 'y'
 	return &Server{
-		Tile: tile,
+		Tile:         tile,
+		broadcastIn:  make(chan interface{}, 10),
+		broadcastOut: make([]chan interface{}, 0),
 	}
 }
 
@@ -28,13 +32,24 @@ func (s *Server) Run(sockPath string) {
 
 	fmt.Println("Listening for new connections on", sockPath)
 
+	go func() {
+		for msg := range s.broadcastIn {
+			for _, ch := range s.broadcastOut {
+				ch <- msg
+			}
+		}
+	}()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("server error establishing connection!", err)
 		}
 		fmt.Println("New connection!")
-		go s.serveConn(conn)
+
+		connChan := make(chan interface{}, 10)
+		s.broadcastOut = append(s.broadcastOut, connChan)
+		go s.serveConn(conn, connChan)
 	}
 }
 
@@ -52,7 +67,7 @@ func NewClientState() ClientState {
 	}
 }
 
-func (s *Server) serveConn(conn net.Conn) {
+func (s *Server) serveConn(conn net.Conn, serverChan chan interface{}) {
 	fmt.Println("Serving connection!", conn.LocalAddr())
 	rpc := NewRPC(conn)
 	rpc.Connect()
@@ -63,25 +78,38 @@ func (s *Server) serveConn(conn net.Conn) {
 	rpc.SendQueue <- ServerMove{X: cs.Player.X, Y: cs.Player.Y}
 
 	fmt.Println("started!")
-	for msg := range rpc.RecvQueue {
-		switch r := msg.(type) {
-		case *ClientReplace:
-			fmt.Println("got a ClientReplace")
-			s.Tile.Cells[r.Y][r.X].Rune = r.Rune
-			rpc.SendQueue <- ServerReplace{
-				X:    r.X,
-				Y:    r.Y,
-				Rune: r.Rune,
+messageLoop:
+	for {
+		select {
+		case msg, ok := <-rpc.RecvQueue:
+			if !ok {
+				break messageLoop
 			}
-		case *ClientMove:
-			x := cs.Player.X + r.X
-			y := cs.Player.Y + r.Y
-			if x < 0 || x >= WIDTH || y < 0 || y >= WIDTH {
-				continue
+			switch r := msg.(type) {
+			case *ClientReplace:
+				fmt.Println("got a ClientReplace")
+				s.Tile.Cells[r.Y][r.X].Rune = r.Rune
+				s.broadcastIn <- ServerReplace{
+					X:    r.X,
+					Y:    r.Y,
+					Rune: r.Rune,
+				}
+
+			case *ClientMove:
+				x := cs.Player.X + r.X
+				y := cs.Player.Y + r.Y
+				if x < 0 || x >= WIDTH || y < 0 || y >= WIDTH {
+					continue
+				}
+				cs.Player.X = x
+				cs.Player.Y = y
+				rpc.SendQueue <- ServerMove{x, y}
 			}
-			cs.Player.X = x
-			cs.Player.Y = y
-			rpc.SendQueue <- ServerMove{x, y}
+		case msg := <-serverChan:
+			switch r := msg.(type) {
+			case (ServerReplace):
+				rpc.SendQueue <- r
+			}
 		}
 	}
 	fmt.Println("finished!")
