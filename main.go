919ba35090f4c10114b032a181743414d2aeeea5
Jeff Wang <jeffawang@gmail.com>
2022-02-19 11:50:31 -0800

    wip: go-flags cmds

--- main.go
+++ main.go
@@ -1,48 +1,34 @@
 package main
 
 import (
-	"flag"
 	"os"
+
+	"github.com/jessevdk/go-flags"
 )
 
 const defaultSockPath = "./test.sock"
 
-type cmdFunc func([]string) error
+type serverOpts struct {
+	sockPath string `short:"s" long:"socket"`
+}
+
+func newServerOpts(args []string) *serverOpts {
+	opts := serverOpts{
+		sockPath: defaultSockPath,
+	}
+	flags.ParseArgs(&opts, args)
+	return &opts
+}
+
+func (s *serverOpts) run() {
+	NewServer(s.sockPath).Run()
+}
 
 func main() {
-	cmd := server
-	args := os.Args[1:]
-	if len(os.Args) > 1 {
-		switch cmdString := os.Args[1]; cmdString {
-		case "server":
-			cmd = server
-			args = os.Args[2:]
-		case "client":
-			cmd = client
-			args = os.Args[2:]
+	if len(os.Args) == 1 {
+		newServerOpts(os.Args[1:]).run()
+	} else {
+		if os.Args[1] == "server" {
 		}
 	}
-	err := cmd(args)
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
-func client(args []string) error {
-	clientFlags := flag.NewFlagSet("client", flag.ExitOnError)
-	socket := clientFlags.String("socket", defaultSockPath, "path to the socket to listen on")
-	clientFlags.Parse(args)
-
-	NewClient(*socket).Run()
-
-	return nil
-}
-
-func server(args []string) error {
-	serverFlags := flag.NewFlagSet("server", flag.ExitOnError)
-	socket := serverFlags.String("socket", defaultSockPath, "path to the socket to listen on")
-	serverFlags.Parse(args)
-
-	NewServer(*socket).Run()
-	return nil
 }
