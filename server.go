12d2b435b395617971f2840d158989fc7428d570
Jeff Wang <jeffawang@gmail.com>
2022-02-18 23:42:41 -0800

    break out connection server to conn struct

--- server.go
+++ server.go
@@ -47,9 +47,10 @@ func (s *Server) Run(sockPath string) {
 		}
 		fmt.Println("New connection!")
 
-		connChan := make(chan interface{}, 10)
-		s.broadcastOut = append(s.broadcastOut, connChan)
-		go s.serveConn(conn, connChan)
+		// connChan := make(chan interface{}, 10)
+		// s.broadcastOut = append(s.broadcastOut, connChan)
+
+		go s.newConn(conn).serve()
 	}
 }
 
@@ -57,39 +58,46 @@ type ClientState struct {
 	Player
 }
 
-func NewClientState() ClientState {
-	return ClientState{
-		Player: Player{
-			X:    WIDTH / 2,
-			Y:    WIDTH / 2,
-			Rune: 'p',
-		},
+type conn struct {
+	srv          *Server
+	broadcastOut chan interface{}
+	rwc          net.Conn
+	rpc          RPC
+}
+
+func (s *Server) newConn(c net.Conn) *conn {
+	ch := make(chan interface{}, 10)
+	s.broadcastOut = append(s.broadcastOut, ch)
+	return &conn{
+		srv:          s,
+		broadcastOut: ch,
+		rwc:          c,
+		rpc:          NewRPC(c),
 	}
 }
 
-func (s *Server) serveConn(conn net.Conn, serverChan chan interface{}) {
-	fmt.Println("Serving connection!", conn.LocalAddr())
-	rpc := NewRPC(conn)
-	rpc.Connect()
+func (c *conn) serve() {
+	fmt.Println("Serving connection!", c.rwc.LocalAddr())
+	c.rpc.Connect()
 
 	cs := NewClientState()
 
-	rpc.SendQueue <- ServerTile{s.Tile}
-	rpc.SendQueue <- ServerMove{X: cs.Player.X, Y: cs.Player.Y}
+	c.rpc.SendQueue <- ServerTile{c.srv.Tile}
+	c.rpc.SendQueue <- ServerMove{X: cs.Player.X, Y: cs.Player.Y}
 
 	fmt.Println("started!")
 messageLoop:
 	for {
 		select {
-		case msg, ok := <-rpc.RecvQueue:
+		case msg, ok := <-c.rpc.RecvQueue:
 			if !ok {
 				break messageLoop
 			}
 			switch r := msg.(type) {
 			case *ClientReplace:
 				fmt.Println("got a ClientReplace")
-				s.Tile.Cells[r.Y][r.X].Rune = r.Rune
-				s.broadcastIn <- ServerReplace{
+				c.srv.Tile.Cells[r.Y][r.X].Rune = r.Rune
+				c.srv.broadcastIn <- ServerReplace{
 					X:    r.X,
 					Y:    r.Y,
 					Rune: r.Rune,
@@ -103,14 +111,24 @@ messageLoop:
 				}
 				cs.Player.X = x
 				cs.Player.Y = y
-				rpc.SendQueue <- ServerMove{x, y}
+				c.rpc.SendQueue <- ServerMove{x, y}
 			}
-		case msg := <-serverChan:
+		case msg := <-c.broadcastOut:
 			switch r := msg.(type) {
 			case (ServerReplace):
-				rpc.SendQueue <- r
+				c.rpc.SendQueue <- r
 			}
 		}
 	}
 	fmt.Println("finished!")
+}
+
+func NewClientState() ClientState {
+	return ClientState{
+		Player: Player{
+			X:    WIDTH / 2,
+			Y:    WIDTH / 2,
+			Rune: 'p',
+		},
+	}
 }
